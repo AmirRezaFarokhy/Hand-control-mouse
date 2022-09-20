2e738594a2dc597beb12bcc4bdc3ee9e9ce4544e
AmirReza Farokhy <113052872+AmirRezaFarokhy@users.noreply.github.com>
2022-09-20 16:10:18 +0430

    edit hand mouse

--- hand_mouse.py
+++ hand_mouse.py
@@ -4,7 +4,7 @@ import pyautogui
 
 class MouseHumanHand:
 
-	def __init__(self, mode=False, maxHands=2, detectionCon=0.5, modelComplexity=1, trackCon=0.5):
+	def __init__(self):
 		self.x_screen, self.y_screen = pyautogui.size()
 		self.x_screen = self.x_screen+600
 		self.mpHands = mp.solutions.hands
@@ -12,8 +12,6 @@ class MouseHumanHand:
 		self.dumb = (4, 2)
 		self.hands = self.mpHands.Hands(False, 2)
 		self.mpDraw = mp.solutions.drawing_utils
-		self.smile_cascade = cv2.CascadeClassifier('smile.xml')
-		self.face_cascade = cv2.CascadeClassifier('face.xml')
 
 	def handsFingure(self,image,draw=True):
 		imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -23,11 +21,10 @@ class MouseHumanHand:
 			for handLms in self.results.multi_hand_landmarks:
 				if draw:
 					self.mpDraw.draw_landmarks(image, 
-											   handLms, 
-											   self.mpHands.HAND_CONNECTIONS)
+								   handLms, 
+							 	   self.mpHands.HAND_CONNECTIONS)
 
 		return image
-
 
 	def detectFingure(self, image, draw=True):
 		movelist = []
@@ -47,18 +44,6 @@ class MouseHumanHand:
 			pyautogui.moveTo(movelist[8][0], movelist[8][1], 0.01)
 
 		return handlist
-
-
-	def DetectSmile(self, frame):
-	    faces = self.face_cascade.detectMultiScale(frame, 1.3, 5)
-	    for (x, y, w, h) in faces:
-	        cv2.rectangle(frame, (x, y), ((x + w), (y + h)), (255, 0, 0), 2)
-	        roi_gray = frame[y:y + h, x:x + w]
-	        roi_color = frame[y:y + h, x:x + w]
-	        smiles = self.smile_cascade.detectMultiScale(roi_gray, 1.8, 20)
-	        for (sx, sy, sw, sh) in smiles:
-	            cv2.rectangle(roi_color, (sx, sy), ((sx + sw), (sy + sh)), (0, 0, 255), 2)
-	    return frame
 
 	def CountFingures(self, handlst):
 		count = 0
